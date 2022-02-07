4e59953ebbd1bafd0640a1e26d8774e85be2de60
Marcelo <marcelo.rds1977@gmail.com>
2022-02-06 21:05:48 -0300

    teste github actions

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #On error no such file entrypoint.sh, execute in terminal - dos2unix .docker\entrypoint.sh
+chown -R www-data:www-data .
 cp .env.example .env
 cp .env.testing.example .env.testing
-chown -R www-data:www-data .
 # composer install
 composer install --no-interaction --prefer-dist --optimize-autoloader -d /var/www
 php artisan config:cache 
